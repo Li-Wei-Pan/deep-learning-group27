dd967c7443b9d353115458eb3025c8696ed7267a
Argony <amoghkadam15@gmail.com>
2026-05-06 16:34:11 +0200

    cross_validation + 200 steps done

--- lstm.py
+++ lstm.py
@@ -38,7 +38,7 @@ def train_model(model, X_train, y_train, X_val, y_val, epochs=200, patience=15):
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     best_val_loss = float('inf')
     patience_counter = 0
-    best_state = None
+    best_state = {k: v.clone() for k, v, in model.state_dict().items()}
 
     for epoch in range(epochs):
         model.train()
@@ -72,6 +72,7 @@ def train_model(model, X_train, y_train, X_val, y_val, epochs=200, patience=15):
 # ---------------- DEFINE AND TUNE WINDOW SIZES PARAMETER _____________----
 
 # Here you go amogh you got this
+#Thank you pookie
 window_size = 10
 
 # ----------------- SPLIT DATA  ACCORDING TO EXPANDING WINDOW SPLIT AND SEQUENCE ACCORDING TO WINDOW SIZE --------------------------
@@ -90,17 +91,17 @@ n_splits = 4
 proportion_train = 0.8
 splits = []
 
-for i in range(n_splits):
-    total_points_split = int((len(x_train_scaled) / n_splits) * (i + 1))
-    n_train_seq = int(total_points_split * proportion_train)
+# for i in range(n_splits):
+    # total_points_split = int((len(x_train_scaled) / n_splits) * (i + 1))
+    # n_train_seq = int(total_points_split * proportion_train)
 
-    # Build sequences over the FULL split so val windows overlap the train boundary
-    X_all, y_all = create_sequences(x_train_scaled[:total_points_split], window_size)
+    # # Build sequences over the FULL split so val windows overlap the train boundary
+    # X_all, y_all = create_sequences(x_train_scaled[:total_points_split], window_size)
 
-    X_train_np, y_train_np = X_all[:n_train_seq], y_all[:n_train_seq]
-    X_val_np,   y_val_np   = X_all[n_train_seq:], y_all[n_train_seq:]
+    # X_train_np, y_train_np = X_all[:n_train_seq], y_all[:n_train_seq]
+    # X_val_np,   y_val_np   = X_all[n_train_seq:], y_all[n_train_seq:]
 
-    splits.append((X_train_np, y_train_np, X_val_np, y_val_np))
+#     splits.append((X_train_np, y_train_np, X_val_np, y_val_np))
 
 # Sanity check for split sizes
 for i in splits:
@@ -108,9 +109,86 @@ for i in splits:
 losses = []
 
 # ------------------- DO CROSSVALIDATION TO FIND BEST HYPERPARAMETER ---------------
+def find_optional_hyperparameters(window_options):
+    best_avg_loss = float('inf')
+    best_window = 10
+
+    for w in window_options:
+        split_losses = []
+        print(f"Testing Window Size: {w}")
+        
+        for i in range(n_splits):
+            total_points_split = int((len(x_train_scaled) / n_splits) * (i + 1))
+            n_train_seq = int(total_points_split * proportion_train)
+
+            # Build sequences over the FULL split so val windows overlap the train boundary
+            X_all, y_all = create_sequences(x_train_scaled[:total_points_split], w)
+
+            X_train_np, y_train_np = X_all[:n_train_seq], y_all[:n_train_seq]
+            X_val_np,   y_val_np   = X_all[n_train_seq:], y_all[n_train_seq:]
+
+            X_t = torch.FloatTensor(X_train_np).unsqueeze(-1)
+            y_t = torch.FloatTensor(y_train_np)
+            X_v = torch.FloatTensor(X_val_np).unsqueeze(-1)
+            y_v = torch.FloatTensor(y_val_np)
+            model = LSTMModel(hidden_size=64)
+
+            val_loss = train_model(model, X_t, y_t, X_v, y_v, epochs=100)
+            split_losses.append(val_loss)
+
+        avg_loss = np.mean(split_losses)
+        print(f"Average Val Loss for window {w}: {avg_loss:.6f}")
+
+        if avg_loss < best_avg_loss:
+            best_avg_loss = avg_loss
+            best_window = w
+
+    #Best Window is 30
+    print(f"Optimial window size fouund: {best_window}")
+    return best_window
+
+best_final_window = find_optional_hyperparameters(window_options=[5,10,20,30,50])
 
 # -------------------- TRAIN MODEL -------------------------------------------------
+def get_final_trained_model(x_scaled, window_size, hidden = 64):
+    
+    X_all , y_all = create_sequences(x_scaled, window_size)
+
+    X_t = torch.FloatTensor(X_all).unsqueeze(-1)
+    y_t = torch.FloatTensor(y_all)
+
+    #Initialize final model
+    final_model = LSTMModel(hidden_size=64)
+    print("Starting Final Training on full dataset")
+    train_model(final_model, X_t, y_t, X_t, y_t, epochs=150)
+    print("Final Model Ready")
+    return final_model
+
+final_lstm = get_final_trained_model(x_train_scaled, window_size= best_final_window)
 
 # ------------------------- PREDICT NEXT 200 STEPS -------------------------------
+def predict_recursion(model, data_source, window_size, steps = 200):
+    model.eval()
+    predictions = []
+
+    #Start with last window of known data
+    current_window = torch.FloatTensor(data_source[-window_size:]).view(1, -1, 1)
+    
+    with torch.no_grad():
+        for _ in range(steps):
+            #Predict one step ahead
+
+            pred = model(current_window)
+            predictions.append(pred.item())
+
+            #Update sliding window
+            new_point = pred.view(1, 1, 1)
+            current_window = torch.cat((current_window[:, 1:, :], new_point), dim=1)
+
+    return np.array(predictions)
+
+recursive_scaled = predict_recursion(final_lstm, x_train_scaled, window_size=best_final_window)
+
+predictions_final = scaler.inverse_transform(recursive_scaled.reshape(-1,1))
 
 # -------------------------- FIND MAE AND MSE ------------------------------------
